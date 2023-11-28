626aea371a1e1b715202b12e71f4c4fb7ec896ca
Nick Cross <ncross@redhat.com>
2023-11-28 12:16:23 +0000

    Fix

--- deploy/base-development.sh
+++ deploy/base-development.sh
@@ -1,35 +1,34 @@
 #!/bin/sh
 
+if [ -z "$JBS_QUAY_IMAGE" ]; then
+    export JBS_QUAY_IMAGE=redhat-appstudio
+fi
+if [ -z "$JBS_WORKER_NAMESPACE" ]; then
+    export JBS_WORKER_NAMESPACE=test-jvm-namespace
+fi
+
 kubectl delete --ignore-not-found deployments.apps hacbs-jvm-operator -n jvm-build-service
 # we don't restart the cache and local storage by default
 # for most cases in development this is not necessary, and just slows things
 # down by needing things to be re-cached/rebuilt
 
 function cleanAllArtifacts() {
-     kubectl delete --ignore-not-found namespaces test-jvm-namespace
+     kubectl delete --ignore-not-found namespaces $JBS_WORKER_NAMESPACE
 }
 
 kubectl delete --ignore-not-found deployments.apps jvm-build-workspace-artifact-cache
 if [ "$1" = "--clean" ]; then
     cleanAllArtifacts
 fi
-if [ -z "$JBS_QUAY_IMAGE" ]; then
-    export JBS_QUAY_IMAGE=redhat-appstudio
-fi
-if [ -z "$JBS_WORKER_NAMESPACE" ]; then
-    export JBS_WORKER_NAMESPACE=jvm-build-workloads
-fi
 
-echo -e "\033[0;32mSetting context to jvm-build-workloads with quay image $JBS_QUAY_IMAGE\033[0m"
+echo -e "\033[0;32mSetting context to $JBS_WORKER_NAMESPACE with quay image $JBS_QUAY_IMAGE\033[0m"
 # Its possible to set context before namespaces have been created.
 kubectl config set-context --current --namespace=$JBS_WORKER_NAMESPACE
 
 
 DIR=`dirname $0`
-# TODO: Set this to JBS_WORKER_NAMESPACE?
-kubectl config set-context --current --namespace=test-jvm-namespace
 
-echo -e "\033[0;32mSecrets...\033[0m"
+#echo -e "\033[0;32mSecrets...\033[0m"
 # kubectl create --dry-run=client -o=yaml secret generic jvm-build-image-secrets --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson | kubectl apply -f -
 # kubectl create --dry-run=client -o=yaml secret generic jvm-build-git-secrets --from-literal .git-credentials="
 # https://$GITHUB_E2E_ORGANIZATION:$GITHUB_TOKEN@github.com
